8c5d82323b75311e6aba80766e7c3ad562f42d46
Manish <manish98ad@gmail.com>
2026-02-26 15:07:36 +0530

    chore: update firebase config with live project keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// You can find this in your Firebase Console -> Project Settings
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCeObBboLXNYV0POjDLTqkfDSMbqTLA95s",
+    authDomain: "nia-nest.firebaseapp.com",
+    projectId: "nia-nest",
+    storageBucket: "nia-nest.firebasestorage.app",
+    messagingSenderId: "5221148078",
+    appId: "1:5221148078:web:94cd55857d7abc3a06943f",
+    measurementId: "G-Y9P1SPBWLX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
